904e695027b60eb8d3164c631159a10a35c2725f
ahmad <issa@beetek.com.au>
2025-01-22 21:20:46 +1100

    update package

--- ffmpeg/ffmpeg.go
+++ ffmpeg/ffmpeg.go
@@ -14,8 +14,8 @@ import (
 	"strconv"
 	"strings"
 
-	"github.com/floostack/transcoder"
-	"github.com/floostack/transcoder/utils"
+	"github.com/ahmadissa/transcoder"
+	"github.com/ahmadissa/transcoder/utils"
 )
 
 // Transcoder ...
